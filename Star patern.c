ff4ffbadfd494ce2028ba1701b154774e07cda20
subbiah-157 <125848016+subbiah-157@users.noreply.github.com>
2023-02-20 15:44:50 +0530

    Create Star patern.c

---
+++ Star patern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,rows;
+    printf("Enter a rows:");
+    scanf("%d", &rows);
+    for(i=0; i<=rows; i++)
+    {
+       for(j=0; j<=i; j++)
+       {
+           printf("* ");
+       }
+       printf("\n");
+    }
+    return 0;
+}
